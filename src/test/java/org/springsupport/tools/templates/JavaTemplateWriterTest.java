9500478d308ce8525fb2d2fb07790e781c9d6701
devdynam0507 <wsnam0507@naver.com>
2022-06-03 11:43:10 +0900

    add java template writer test

--- src/test/java/org/springsupport/tools/templates/JavaTemplateWriterTest.java
+++ src/test/java/org/springsupport/tools/templates/JavaTemplateWriterTest.java
@@ -1,7 +1,10 @@
 package org.springsupport.tools.templates;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaTemplateWriterTest {
 
+    private JavaTemplateWriter templateWriter = new JavaTemplateWriter();
 }

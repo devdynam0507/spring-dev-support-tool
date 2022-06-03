13ebbe53eb9286b8c8ee62de1bd422cc52f6984d
devdynam0507 <wsnam0507@naver.com>
2022-06-03 11:44:15 +0900

    implements java template writer concrete

--- src/main/java/org/springsupport/tools/templates/JavaTemplateWriter.java
+++ src/main/java/org/springsupport/tools/templates/JavaTemplateWriter.java
@@ -1,5 +1,7 @@
 package org.springsupport.tools.templates;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
@@ -7,19 +9,36 @@ import org.springsupport.tools.templates.files.TemplateContent;
 
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotNull;
+
 @Component
 @RequiredArgsConstructor
 public class JavaTemplateWriter implements TemplateWriter {
 
 	@Override
-	public void write(List<TemplateContent> renderedContents) {
+	public void write(List<TemplateContent> renderedContents, String packageName) {
 		String currentDir = System.getProperty("user.dir");
-		write(currentDir, renderedContents);
+		write(packageName, currentDir, renderedContents);
 	}
 
 	@Override
-	public void write(String rootPath, List<TemplateContent> renderedContents) {
-		System.out.println("");
+	public void write(String packageName, String rootPath, List<TemplateContent> renderedContents) {
+		try {
+			for(TemplateContent templateContent : renderedContents) {
+				FileWriter fileWriter = getFileWriter(rootPath + "/" + packageName, templateContent.getTemplateName());
+				fileWriter.write(templateContent.getContent());
+				fileWriter.flush();
+				fileWriter.close();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private FileWriter getFileWriter(@NotNull final String rootDirectory, String fileNameWithDirectory) throws IOException {
+		FileWriter fileWriter = new FileWriter(rootDirectory + fileNameWithDirectory);
+		return fileWriter;
 	}
 
 }

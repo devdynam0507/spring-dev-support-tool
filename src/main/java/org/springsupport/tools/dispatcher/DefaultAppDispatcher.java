1e3f40b1ec595598a6e5ef6baa62a0fea5636748
devdynam0507 <wsnam0507@naver.com>
2022-06-03 14:25:44 +0900

    remove unused import, variables

--- src/main/java/org/springsupport/tools/dispatcher/DefaultAppDispatcher.java
+++ src/main/java/org/springsupport/tools/dispatcher/DefaultAppDispatcher.java
@@ -1,14 +1,10 @@
 package org.springsupport.tools.dispatcher;
 
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springsupport.tools.argument.dto.ArgumentPipelineContext;
 import org.springsupport.tools.argument.resolver.ArgumentResolver;
-import org.springsupport.tools.lang.SupportLanguage;
 import org.springsupport.tools.templates.TemplateRenderer;
 import org.springsupport.tools.templates.TemplateWriter;
 import org.springsupport.tools.templates.files.TemplateContent;
@@ -19,9 +15,6 @@ import javax.validation.constraints.NotNull;
 @Component
 @RequiredArgsConstructor
 public class DefaultAppDispatcher implements AppDispatcher {
-
-	private static final String TEMPLATE_RENDERER_NAME = "TemplateRenderer";
-    private static final String TEMPLATE_WRITER_NAME = "TemplateWriter";
 
 	private final ArgumentResolver argumentResolver;
     private final TemplateStrategySupport templateStrategySupport;

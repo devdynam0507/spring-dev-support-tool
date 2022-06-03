e7af0594941f9f82eb3fd3df3f8ba3adaeb27174
devdynam0507 <wsnam0507@naver.com>
2022-06-03 14:26:59 +0900

    implements main

--- src/main/java/org/springsupport/tools/SpringSupportToolsApp.java
+++ src/main/java/org/springsupport/tools/SpringSupportToolsApp.java
@@ -1,9 +1,16 @@
 package org.springsupport.tools;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springsupport.tools.dispatcher.AppDispatcher;
+
 public class SpringSupportToolsApp {
 
 	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppDependencyConfiguration.class);
+		AppDispatcher appDispatcher = context.getBean(AppDispatcher.class);
 
+		appDispatcher.doDispatch(args);
 	}
 
 }
